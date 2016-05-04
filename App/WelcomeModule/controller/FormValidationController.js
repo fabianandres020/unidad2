a24c6f20983db66fe9c9f37b51ef19c0d6d7b017
Fabian Andres <Fabian Andres>
2016-05-04 20:39:57 -0300

    V3

--- App/WelcomeModule/controller/FormValidationController.js
+++ App/WelcomeModule/controller/FormValidationController.js
@@ -7,6 +7,14 @@
 var app=angular.module('LoginFormModule',[]);
 
 //controller
-app.controller('LoginFormController',function($socpe,$http){
-    
+app.controller('LoginFormController',function($scope,$http){
+    $scope.formModel={};
+
+
+    //funcion que llama al submit
+    $scope.onSubmit=function(){
+      console.log("LLegue,!Primer submit con AngularJs");
+        console.log($socpe.formModel);
+        //llamadas Http
+    };
 })
