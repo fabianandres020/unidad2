a74bafc26979fb91b008e4cb528a633915cdbf34
Fabian Andres <Fabian Andres>
2016-05-04 20:04:28 -0300

    Agregando el from

--- App/WelcomeModule/controller/FormValidationController.js
+++ App/WelcomeModule/controller/FormValidationController.js
@@ -2,4 +2,11 @@
  * Created by Fabian on 02-05-2016.
  */
 
+//Generar modulo
 
+var app=angular.module('LoginFormModule',[]);
+
+//controller
+app.controller('LoginFormController',function($socpe,$http){
+    
+})
